3480e1e2fc810826133fda51519b10cb2fa0b3a2
greenturtle1134 <daniel.y.fu@gmail.com>
2022-02-13 13:31:42 -0600

    Added Turn Game 5

--- turngame3.js
+++ turngame3.js
@@ -5,7 +5,10 @@ app = Vue.createApp({
           used: [false, false, false],
           diceText: "Dice",
           turns: 0,
-          rolls: 0
+          rolls: 0,
+          five: false,
+          currRandom: Math.floor(Math.random()*3),
+          nextRandom: Math.floor(Math.random()*3)
         }
     },
     methods: {
@@ -13,6 +16,8 @@ app = Vue.createApp({
           this.used[i] = true;
           if(this.count == 0) {
             this.used = [false, false, false];
+            this.currRandom = this.nextRandom;
+            this.nextRandom = Math.floor(Math.random()*3);
           }
           this.diceText = "Dice";
           this.rolled = false;
@@ -20,21 +25,37 @@ app = Vue.createApp({
         },
         dice() {
           let choice = [];
-          for(f in this.used) {
-            if(this.used[f]){
-              choice.push(f);
+          if(this.count == 3 && this.five) {
+            for(f in this.used) {
+              if(f != this.currRandom){
+                choice.push(f);
+              }
+            }
+            if(!this.rolled) {
+              let chosen = Math.floor(Math.random()*2);
+              this.diceText = "Go " + this.names[choice[chosen]];
+              this.rolled = true;
+              this.used = [false, false, false];
+              this.used[chosen] = true;
+              this.rolls++;
             }
           }
-          if(choice.length == 2 && !this.rolled) {
-            this.diceText = "Go " + this.names[choice[Math.floor(Math.random()*2)]];
-            this.rolled = true;
-            this.used = [false, false, false];
-            this.rolls++;
+          else {
+            for(f in this.used) {
+              if(this.used[f]){
+                choice.push(f);
+              }
+            }
+            if(choice.length == 2 && !this.rolled) {
+              this.diceText = "Go " + this.names[choice[Math.floor(Math.random()*2)]];
+              this.rolled = true;
+              this.used = [false, false, false];
+              this.rolls++;
+            }
           }
         }
     },
     computed: {
-      // a computed getter
       count() {
         let i = 0;
         for(f in this.used) {
